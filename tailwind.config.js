0711923691bd2e73b8d30aae944295611357d998
ethanntieu <ethan.tieu@gmail.com>
2024-11-29 23:11:55 -0800

    fixed tailwin config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-Copy/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -9,4 +9,3 @@ export default {
   },
   plugins: [],
 }
-
