59f2dfa4e7af5967752dd77aaa134f62ef3fb4dd
ethanntieu <ethan.tieu@gmail.com>
2024-11-29 23:53:47 -0800

    general updates

--- src/data/education.js
+++ src/data/education.js
@@ -4,6 +4,7 @@ export const educationData = [
       institution: "University of Washington",
       location: "Seattle, WA",
       duration: "2023 - 2026",
-      description: "3.83/4.0 GPA | Relevant Coursework: Hardware Software Interface, Discrete Math I, Intro to Digital Design, Data Structures and Paralellism, Intro to Signal Conditioning, Linear Algebra",
+      description: "3.83/4.0 GPA, Dean's List 23/24 academic year, Member of the Husky Tennis Club, Husky Satellite Lab, and UW Solar Vehicle Team",
+      classes: "Relevant Coursework: Hardware Software Interface, Discrete Math I, Intro to Digital Design, Data Structures and Paralellism, Intro to Signal Conditioning, Linear Algebra",
     },
   ];
