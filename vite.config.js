b1ca3eaeaefed7a7a2a2f01786b754db81879668
ethanntieu <ethan.tieu@gmail.com>
2024-12-01 23:33:31 -0800

    new repo name updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ethanntieu.github.io/', // Replace with your repository name
+  base: '/', // Replace with your repository name
 })
